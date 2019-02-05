3da7b313e44f2b6878bade5f13a724638cd169bf
Dmitry <DetSimen@users.noreply.github.com>
2019-02-05 16:43:22 +0700

    Add files via upload

--- TTimerList.cpp
+++ TTimerList.cpp
@@ -1,19 +1,12 @@
-﻿#include "Arduino.h"
+﻿#include <Arduino.h>
 #include "TTimerList.h"
 
-
-
-static bool InRange(int value, int min, int max)
-{
-	return (value >= min) AND(value <= max);
-}
-
-TTimerList TimerList; 
-
+// создается пустой, остановленный список щёччиков 
+TTimerList TimerList(MAXTIMERSCOUNT);
 
 /// Настройка таймеров для первого использования
 /// на срабатывание каждую 1 миллисекунду
-/// для Uno, Nano и прочих Micro с ATMega328, ATmega168, и на Mega2560  работает на таймере #0
+/// для Uno, Nano и прочих Micro с ATMega168, ATMega328, и на Mega2560  работает на таймере #0
 /// на Atmega8 работает на таймере 1, на таймере 0 нет прерывания по совпадению
 /// на других протестировать нет возможности
 
@@ -31,7 +24,7 @@ void TTimerList::Init()
 	SREG = oldSREG;
 
 }
-#elif defined(__AVR_ATmega328P__)  || defined(__AVR_ATmega168__)
+#elif defined(__AVR_ATmega328P__)
 void TTimerList::Init()
 {
 	byte oldSREG = SREG;
@@ -45,7 +38,24 @@ void TTimerList::Init()
 	SREG = oldSREG;
 
 }
-#elif defined(__AVR_ATMEGA8__)
+
+#elif defined(__AVR_ATmega168__)
+void TTimerList::Init()
+{
+	byte oldSREG = SREG;
+	cli();
+
+	TCCR0A = TCCR0A & 0b11111100;
+	OCR0A = TIMER0_ONE_MS;
+	TIMSK0 |= 0x3;
+	TIFR0 = TIFR0 | 0x2;
+
+	SREG = oldSREG;
+
+}
+
+
+#elif defined(__AVR_ATmega8__)
 void TTimerList::Init()
 {
 	byte oldSREG = SREG;
@@ -58,7 +68,7 @@ void TTimerList::Init()
 	TCCR1B |= (1 << WGM12);
 //	TCCR1B |= (1 << CS11) | (1 << CS10);
 	
-	TIMSK1 |= (1 << OCIE1A); 
+	TIMSK |= (1 << OCIE1A); 
 
 
 	SREG = oldSREG;
@@ -66,197 +76,19 @@ void TTimerList::Init()
 }
 #endif
 
-TTimerList::TTimerList()
-{
-	count = 0;
-	memset(Items,0,sizeof(Items[0])*MAXTIMERSCOUNT);
-	active = false;
-}
 
-THandle TTimerList::Add(PVoidFunc AFunc, long timeMS)
-{
-	for (THandle i = 0; i < MAXTIMERSCOUNT; i++)
-	{
-		if (Items[i].CallingFunc == NULL)         // пробегаемся по списку таймеров
-		{                                         // если нашли пустую дырку, добавляем таймер
-			byte sreg = SREG; cli();
-			Items[i].CallingFunc	= AFunc;
-			Items[i].InitCounter	= timeMS;
-			Items[i].WorkingCounter = timeMS;
-			Items[i].Active          = true;
-			count++;
-			if (NOT active)  AllStart();
-			SREG = sreg;
-			return i;
-		}
-	}
-	return INVALID_HANDLE;                                   // если нет - вернем код ашыпки (-1) 
-}
+#if defined(__AVR_ATmega8__)
 
-THandle TTimerList::AddSeconds(PVoidFunc AFunc, word timeSec)
-{
-	return Add(AFunc, 1000L*timeSec);          // оналогично для секунд 
-}
-
-THandle TTimerList::AddMinutes(PVoidFunc AFunc, word timeMin)
-{
-	return Add(AFunc, timeMin*60L*1000L);     // оналогично для минут
-}
-
-
-
-bool TTimerList::CanAdd() const
-{
-	for (byte i = 0; i < MAXTIMERSCOUNT; i++)
-	{
-		if (Items[i].CallingFunc != NULL) continue;  // если в списке есть пустые места, куда можно добавить таймер
-		return true;                                 // вернем true
-	}
-	return false;                                    // если нет - то false
-}
-
-bool TTimerList::IsActive() const
-{
-	return active;                                   // если хоть один таймер активен, вернет true, если все остановлены - false
-}
-
-void TTimerList::Delete(THandle hnd)                 // удалить таймер с хэндлом hnd.     
-{
-	if (InRange(hnd, 0, MAXTIMERSCOUNT - 1))
-	{
-		byte sreg = SREG; cli();
-		Items[hnd].CallingFunc		= NULL;
-		Items[hnd].InitCounter		= 0;
-		Items[hnd].WorkingCounter	= 0;
-		Items[hnd].Active           = false;
-		if (count > 0)  count--;
-		if (count==0) AllStop();                        // если все таймеры удалены, остановить и цикл перебора таймеров
-		SREG = sreg;
-	}
-}
-
-/// эта функция вызывается при аппаратном срабатывании таймера каждую миллисекунду
-/// запуская цикл декремента интервалов срабатывания
-
-void TTimerList::Step(void)
-{
-	if (NOT active) return;       // если все таймеры остановлены, то и смысла нет
-	
-	byte sreg = SREG;	// запомним состояние прерываний
-	cli();                  // чтоб никто не помешал,  запрещаем прерывания
-	
-			// сопсно рабочий цикл                      
-	for (THandle i = 0; i < MAXTIMERSCOUNT; i++)   // пробегаем по всему списку таймеров
-	{
-		if (Items[i].CallingFunc == NULL)  continue;  	// если функция-обрабоччик не назначена, уходим на следующий цикл 
-		if (NOT Items[i].Active)           continue; 	// если таймер остановлен - тоже
-		if (--Items[i].WorkingCounter > 0) continue;  	// уменьшаем на 1 рабочий счетчик
-                sei(); 
-		Items[i].CallingFunc();                       	// если достиг 0, вызываем функцию-обработчик
-		cli();
-		Items[i].WorkingCounter = Items[i].InitCounter; // и записываем в рабочий счетчик начальное значение для счета сначала
-	}							// прерывания ЗАПРЕЩЕНЫ
-
-	SREG = sreg;	                                  	// теперь и прерывания можно восстановить как было
-}
-
-void TTimerList::AllStart()                              
-{
-	if (NOT active) Init();                              // при добавлении первого таймера, инициализируем аппаратный таймер
-	active = true;
-}
-
-void TTimerList::AllStop()
-{
-	active = false;                                   // остановить все таймеры
-}
-
-void TTimerList::TimerStop(THandle hnd)
-{
-	if (InRange(hnd, 0, MAXTIMERSCOUNT-1))
-	{
-		Items[hnd].Active = false;                    // остановить таймер номер hnd
-	}
-}
-
-void	TTimerList::TimerReset(THandle hnd)	// для таймера hnd начать счёт с начала
-{
-	if (isValid(hnd))
-	{
-		byte sreg = SREG;
-		cli();
-		TimerStop(hnd);
-		Items[hnd].WorkingCounter = Items[hnd].InitCounter;
-		TimerStart(hnd);
-		SREG = sreg;
-	}
-}
-
-
-bool TTimerList::TimerActive(THandle hnd)
-{
-	if (NOT InRange(hnd, 0, MAXTIMERSCOUNT - 1)) return false;
-	else return Items[hnd].Active;
-}
-
-void TTimerList::TimerStart(THandle hnd)              // запустить остановленный таймер номер hnd
-{
-	if (isValid(hnd))
-	{
-		if (Items[hnd].Active)                   return;
-		byte old = SREG; 
-		cli();
-		Items[hnd].WorkingCounter = Items[hnd].InitCounter;  // и начать отсчет интервала сначала
-		Items[hnd].Active = true;
-		SREG = old;
-	}
-}
-
-void TTimerList::TimerResume(THandle hnd)                 // продолжить счёт таймера после паузы
-{
-	if (isValid(hnd)) Items[hnd].Active = true;
-}
-
-void TTimerList::TimerPause(THandle hnd)				// поставить таймер на паузу
-{
-	if (isValid(hnd)) Items[hnd].Active = false;
-}
-
-bool TTimerList::isValid(THandle hnd)					// private функция, cнаружи не видна
-{
-	return (InRange(hnd, 0, MAXTIMERSCOUNT) AND (Items[hnd].CallingFunc != NULL));
-}
-
-void TTimerList::TimerNewInterval(THandle hnd, long newinterval)	// назначить таймеру новый интервал
-{
-	if (isValid(hnd))
-	{
-		byte sreg = SREG;
-		cli();
-		TimerStop(hnd);
-		Items[hnd].InitCounter = newinterval;
-		TimerStart(hnd);
-		SREG = sreg;
-	}
-}
-
-#if defined(__AVR_ATMEGA8__)
-
-ISR(TIMER1_COMPA_vect)
-{
+ISR(TIMER1_COMPA_vect){
 	OCR1A = _1MSCONST;
-	TCNT1 = 0x0000;
-	TimerList.Step();
+	TCNT1 = 0xFFFF;		// чтоб на следующем шаге сработало переполнение и посчитался millis
+	TimerList.Tick();
 }
 #else
-ISR(TIMER0_COMPA_vect)
-{
-
-	TimerList.Step();
-
-	TCNT0 = 0xFF;
+ISR(TIMER0_COMPA_vect){
+	TCNT0 = 0xFF;		// чтоб на следующем шаге сработало переполнение и посчитался millis
+	TimerList.Tick();
 }
 #endif
-
 
 
--- TTimerList.h
+++ TTimerList.h
@@ -1,121 +1,288 @@
 ﻿#pragma once
-#include "Arduino.h"
+#include <Arduino.h>
+#include <Consts.h>
 
+using bsize_t = uint8_t;
 using THandle = int8_t;
 
-const THandle INVALID_HANDLE = -1;
-
-#define AND &&
-#define OR ||
-#define NOT !
-
-
-using	PVoidFunc = void(*)(void);      // указатель на функцию без параметров, ничего не возвращающую: void AnyFunc(void)  
+static const int8_t INVALID_HANDLE = -1;
 
 
 #ifdef __AVR_ATmega2560__
- #define     	MAXTIMERSCOUNT		16		// 	Максимальное число зарегистрированных таймеров для Mega2560 - 16
+ #define    MAXTIMERSCOUNT	16		// 	Максимальное число зарегистрированных таймеров для Mega2560 - 16
  #define	TIMER0_ONE_MS	247
 #endif
 
-#ifdef __AVR_ATMEGA8__
- #define	 MAXTIMERSCOUNT		8
+#ifdef __AVR_ATmega8__
+ #define	 MAXTIMERSCOUNT	8
  #define	 _1MSCONST		F_CPU/8/1000 //	задержка для 1 миллисекунды  12Мгц - 1500, 16Мгц - 2000 минус накладные расходы 
 											//  между прерываниями 16 Мгц процессор выполнит примерно 10000 команд
 #endif
 
+
 #ifdef __AVR_ATmega328P__
-#define  MAXTIMERSCOUNT		12		// 	Максимальное число зарегистрированных таймеров для Uno - 12
-#define	 TIMER0_ONE_MS	    247
+#define  MAXTIMERSCOUNT		10		// 	Максимальное число зарегистрированных таймеров для прочих Uno - 10
+#define	 TIMER0_ONE_MS		245
+											
 #endif
 
 #ifdef __AVR_ATmega168__
-#define  MAXTIMERSCOUNT 	8		// 	Максимальное число зарегистрированных таймеров для прочих 168 - 8
-#define	 TIMER0_ONE_MS	    247
+#define  MAXTIMERSCOUNT		8		// 	Максимальное число зарегистрированных таймеров для прочих Old Uno - 8
+#define	 TIMER0_ONE_MS		240
+											
 #endif
 
 
 
-using	THandle = int8_t;					// можно замонстрячить от 0 до максимум 127 таймеров, только зачем?
+using pvfCallback = void(*)(void); // тип pointer to void function Callback
 
+struct TCounterDown;				// опережающее обявление структуры TCounterDown
+using  PCounterDown = TCounterDown *;// и указателя на нее
 
-#pragma pack(push,1)
-struct TCallStruct					    // внутренняя структура для хранения КАЖДОГО таймера
-{
-	PVoidFunc	CallingFunc;			// функция, которую нужно вызвать при срабатывании
-	long		InitCounter;			// заданное кол-во миллисекунд
-	long		WorkingCounter;			// рабочий счетчик. после начала счёта сюда копируется значение из InitCounter
-	bool       	Active;	  			    // и каждую миллисекунду уменьшается на 1. При достиженнии 0 вызывается функция 
-							            // CallingFunc, и снова записывается значение из InitCounter. Счёт начинается сначала. :)
-};
-#pragma pack(pop)
-using PCallStruct = TCallStruct *;
+#pragma pack(push,1)				// выравнивание полей структуры - 1 байт
 
-class TTimerList
-{
-private:
-	TCallStruct		Items[MAXTIMERSCOUNT];
-	void			Init();
-	bool			active;
-	byte			count;
+class TCounterDown {
+protected:
+	bool		fActive;        // 1 byte   true - если счетчик считает, false, если остановлен
+	uint32_t	fWorkCount;	    // 4 bytes	Рабочий счетчик, уменьшается на 1 каждый Tick()
+	uint32_t	fInitCount;		// 4 bytes  Начальный счетчик, хранит значение от которого считать до 0
+	pvfCallback fCallback;		// 2 bytes  Адрес функции, которая вызовется, когда счётчик досчитает до 0
+								// size of this struct = 11 bytes;
 
-// проверка, что таймер существует, его номер правильный
-// и callback функция присвоена
-	bool			isValid(THandle hnd);				
+	// гарантированно очистить (заполнить нулями) все поля структуры
+	void		Clear(void) { memset(this, 0, sizeof(TCounterDown)); };
 
+	// пустой конструктор, не разрешен
+	TCounterDown() {};
 public:
-	TTimerList();
-
-
-	THandle      Add(PVoidFunc AFunc, long timeMS);	        // функция AFunc, которую надо вызвать через timeMS миллисекунд
-
-	THandle		 AddStopped(PVoidFunc AFunc, long AMS) {	// создать таймер не запуская его (в остановленном состоянии)
-	   THandle result = Add(AFunc, AMS);					// AMS - время в миллисекундах	
-	   if (result>=0) TimerStop(result);
-	   return result;
+	
+	// конструктор. Принимает первоначальное значение, которое потом будет уменьшаться при каждом вызове 
+	// Tick() и указатель на void функцию обратного вызова.
+	TCounterDown(uint32_t aTimeMS, pvfCallback aCallback) {
+		Clear();							//очистить все поля
+		fInitCount = fWorkCount = aTimeMS;  // задать начальные значения 
+		fCallback = aCallback;
+		fActive = true;						// и сразу разрешить запуск
 	}
 
-	THandle      AddSeconds(PVoidFunc AFunc, word timeSec); // то же, только интервал задается в секундах. 
-	THandle      AddMinutes(PVoidFunc AFunc, word timeMin); // то же, только интервал задается в минутах. 
-															// исключительно для удобства создания длинных интервалов 
-// функции для списка в целом
+	// остановить счетчик (поставить на паузу)
+	inline void Stop(void) {
+		fActive = false; 
+	};
 
-		inline bool  	CanAdd() const;                // если можно добавить таймер, вернет true
+	// продолжить считать с того же места, где остановили	
+	inline void Start(void) { 
+		fActive = true; 
+	};		
 
-		inline bool	IsActive() const;                  // true, если хоть один таймер запущен
+	// сбросить счетчик, считать сначала
+	inline void Reset(void) { 
+		Stop(); 
+		fWorkCount = fInitCount; 
+		Start(); 
+	}; 
 
-		void		Delete(THandle hnd);               // удалить таймер hnd
+	// отдает true - если счетчик запущен и считает, и false, если остановлен
+	inline bool isActive(void) const { 
+		return fActive; 
+	};			
 
-		void		Step(void);                
+	// отдает true - если функция Callback не назначена
+	inline bool isEmpty(void) const { 
+		return (fCallback == NULL); 
+	};
 
-		void		AllStart();                        // запустить рабочий цикл перебора таймеров
+	// установить новый интервал для счета
+	void setInterval(uint32_t anewinterval) { 
+		Stop(); 
+		fInitCount = fWorkCount = anewinterval; 
+		Start(); 
+	};
 
-		void		AllStop();                         // выключить все таймеры, TimerList остановлен
+	// Оператор постдекремента. Уменьшает рабочий счетчик на 1 за вызов, если он не остановлен. Если счетчик дошел до 0, 
+	// и назначена функция Callback, то она вызывается
+	TCounterDown &operator --(int) {
 
-		inline byte	Count() const {return count; };    // счетчик добавленных таймеров, всего. Можно не использовать, просто проверять возможность
-		 	                                            // добавления, вызывая ф-цию CanAdd();
+		uint8_t sreg = SREG;   // на всякий случай запомним состояние прерываний
 
-		inline byte MaxTimersCount() const { return MAXTIMERSCOUNT; }; // максимальное число таймеров
+		cli();				// запрещаем прерывания
 
-
-//  функции для одного конкретного таймера, заданного его номером (THandle)
-
-		void	TimerPause(THandle hnd);	//приостановить таймер hnd
-
-		void	TimerResume(THandle hnd);	// продолжить счёт для таймера hnd с остановленного места
-
-		void	TimerReset(THandle hnd);	// для таймера hnd начать счёт с начала
-
-		void	TimerStop(THandle hnd);		// остановить отдельный таймер 
-
-		bool	TimerActive(THandle hnd);   // запущен ли конкретный таймер
-
-		void    TimerStart(THandle hnd);    // запустить отдельный таймер (после остановки) 
+		if ((isActive()) && (!isEmpty()) && ((--fWorkCount) == 0)) {  // если счетчик досчитал до 0
+			fWorkCount = fInitCount; // перезагружаем рабочий счётчик начальным значением, чтобы считать заново
+			sei();				// разрешаем прерывания
+			fCallback();		// и вызываем нашу фунцию обратного вызова
+		}
 		
-		void	TimerNewInterval(THandle hnd, long newinterval); // назначить таймеру hnd новый интервал
+		SREG = sreg;     // восстанавливаем прерывания
+		return *this;
+	}
+
+	// то же самое в виде функции, а не оператора
+	void Tick(void) { (*this)--; };  // просто вызываем оператор постдекремента
+
 };
 
+#pragma pack(pop)
 
 
-extern TTimerList TimerList;                            // переменная будет видна в основном скетче
+class TTimerList;				// опережающее обьявление класса 
+using PTimerList = TTimerList *; // и указателя на него
+
+extern TTimerList TimerList;   // глобальная переменная нашего списка счетчиков
+
+
+
+class TTimerList   {
+protected:
+	bool		factive;	// Активность всего списка, если false - все таймеры остановлены
+	bsize_t		fsize;		// Размер списка
+	bsize_t		fcount;		// Количество добавленных таймеров
+
+	PCounterDown *Items;	// массив указателей на счетчики TCounterDown
+
+	// отдает true если THandle счётчика правильный [0..fsize-1]
+	// и сам счетчик не удален 
+	bool isValid(THandle ahandle) {
+		return ((ahandle >= 0) && (ahandle < fsize)) && (Items[ahandle] != NULL);
+	}
+
+	// начальная инициализация аппаратного таймера
+	// реализация в cpp файле для разных камней разная
+	void Init(void);
+
+	// пустой конструктор, не разрешен, спрятан в protected
+	TTimerList(){};
+
+public:
+	// создает пустой список размера asize, для хранения счетчиков
+	// при создании забивает список NULL-ами
+	TTimerList(uint8_t asize) {
+		Items = new PCounterDown[fsize=asize];// попытаемся хапнуть память под массив указателей на счётчики          
+		if (Items == NULL) return;			  // если памяти нет, просто уходим, всё равно потом всё скрашится
+		for (bsize_t i = 0; i < fsize; i++) 
+			 Items[i] = NULL;				  // забиваем выделенную память нулями
+		factive = false;					  // пока ни один счетчик не добавлен, список неактивен	
+		fcount = 0;							  // и число счётчиков пока == 0
+	};
+
+/* for oversmart boys: uncomment this */
+//
+//	~TTimerList() {
+//		for (bsize_t i = 0; i < fsize; i++) {
+//			if (isValidHandle(i)) delete Items[i];
+//		}
+//		delete[] Items;
+//	}
+
+	// добавить счетчик в список
+	// ainterval задается в миллисекундах
+	// acallback - адрес функции, которая вызывается, когда счетчик досчитает до 0
+	// 
+	// возвращает Handle щёччика, или специальное значение INVALID_HANDLE, если 
+	// добавить не удалось
+	THandle Add(uint32_t ainterval, pvfCallback acallback) {
+		
+		if (fcount == (fsize - 1)) return INVALID_HANDLE; // список заполнен до отказа, мест нет
+
+		PCounterDown counter = new TCounterDown(ainterval, acallback); // попытаемся хапнуть память под щёччик
+		
+		if (counter == NULL) return INVALID_HANDLE;  // не удалось, уходим с ошибкой
+
+		if (fcount == 0) {		// если добавляемый счетчик - первый, 
+			Init();				// то сначала инициализируем аппаратный таймер
+			Start();			// и разрешаем запустить весь этот цыкал перебора
+		}
+
+		for (THandle i = 0; i < fsize; i++) {
+			if (Items[i] == NULL) {   // ищем свободную "дырку" в массиве Items
+				Items[i] = counter;	  // если нашли - пхаем туда созданный щёччик	
+				fcount++;			  // кол-во счетчиков увеличиваем на 1	
+				return i;			  // возвращаем индекс добавленного счётчика как его Handle
+			}
+		}
+
+		return INVALID_HANDLE;		  // ну, а если места не нашлось - извините. 
+
+	}
+
+	// то же, но счетчик создается в остановленном состоянии
+	// для запуска нужно потом вызвать TimerList.Start(hnd)
+	// 
+	THandle AddStopped(uint32_t ainterval, pvfCallback acallback) {
+		THandle h = Add(ainterval, acallback);
+		if (h != INVALID_HANDLE) Stop(h);
+		return h;
+	}
+
+	// запустить цикл перебора счетчиков
+	inline void Start(void) { 
+		factive = true; 
+	};
+
+	// остановить цикл перебора счётчиков
+	inline void Stop(void) { 
+		factive = false; 
+	};
+
+	// отдает true - если цикл перебора запущен
+	inline bool isActive(void) const { 
+		return factive; 
+	};
+
+	// функции для работы с конкретным счётчиком в списке, по его Handle
+
+	// запустить счётчик с номером hnd
+	void Start(THandle hnd) {
+		if (isValid(hnd)) Items[hnd]->Start();
+	}
+
+	// остановить счётчик hnd
+	void Stop(THandle hnd) {
+		if (isValid(hnd)) Items[hnd]->Stop();
+	}
+
+	// Перезапустить счётчик hnd сначала
+	void Reset(THandle hnd) {
+		if (isValid(hnd)) Items[hnd]->Reset();
+	}
+
+	// Установить счетчику hnd новый интервал счёта
+	void setNewInterval(THandle hnd, uint32_t anewinterval) {
+		if (isValid(hnd)) Items[hnd]->setInterval(anewinterval);
+	}
+	
+	// проверить, активен ли счётчик hnd
+	bool isActive(THandle hnd) {
+		if (isValid(hnd)) return Items[hnd]->isActive();
+		return false;
+	}
+
+	// остановить и удалить счётчик hnd из массива
+	void Delete(THandle hnd) {
+		if (!isValid(hnd)) return;
+		PCounterDown cntptr = Items[hnd];
+		cntptr->Stop();
+		delete(cntptr);
+		Items[hnd] = NULL;
+		if (--fcount == 0) Stop(); // Если счётчиков не осталось - остановить цикл перебора
+	}
+
+	// отдает количество добавленных в список щёччиков
+	bsize_t getCount() const {
+		return fcount;
+	};
+
+
+	// сюда приходят тики от аппаратного таймера. Если список активен, запускается цикл
+	// перебора счетчиков в списке.  На каждом тике каждый добавленный счётчик уменьшается на 1
+	void Tick(void) {
+
+		if (isActive()) {
+			for (bsize_t i = 0; i < fsize; i++)  
+				if (isValid(i)) (*Items[i])--;
+		}
+
+	}
+};
+
 
